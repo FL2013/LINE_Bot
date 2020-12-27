57204f10905766c8a23c90c1a8f0f6372048e6b1
FL2013 <62010224+FL2013@users.noreply.github.com>
2020-12-27 11:14:21 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -125,4 +125,4 @@ if __name__ == "__main__":
      
   
      port = os.environ.get("PORT", 8000)
-     app.run(host="0.0.0.0", port=4040, debug=True)
+     app.run(host="0.0.0.0", port=port, debug=True)
